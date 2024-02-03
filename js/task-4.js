0ce1db6c1865de623161371d47aa7e3d18330b02
Viktor1907 <vikvelik@bigmir.net>
2024-02-03 19:06:46 +0200

    task3end

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+
+  if (email.trim() === "" || password.trim() === "") {
+    alert("Будь ласка, заповніть усі поля");
+    return;
+  }
+
+  console.log("Email:", email);
+  console.log("Password:", password);
+
+  event.target.reset();
+}
 
