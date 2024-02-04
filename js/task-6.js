98e72f64ef9dc0227c354a2efdb680ae2749f6a3
Viktor1907 <vikvelik@bigmir.net>
2024-02-04 11:09:04 +0200

    task6end

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,39 @@
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-action="create"]');
+const destroyBtn = document.querySelector('button[data-action="destroy"]');
+const boxesContainer = document.getElementById("boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(input.value);
+  if (amount < 1 || amount > 100) {
+    alert("Please enter a number between 1 and 100");
+    return;
+  }
+
+  destroyBoxes();
+
+  const boxes = [];
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = size + "px";
+    box.style.height = size + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    boxes.push(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
