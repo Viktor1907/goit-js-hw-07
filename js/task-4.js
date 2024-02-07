c0cae1d7cab64cd794d52f4dfc11e889cfac8fc9
Viktor1907 <vikvelik@bigmir.net>
2024-02-07 18:52:08 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -5,17 +5,14 @@ loginForm.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  const email = event.target.elements.email.value;
-  const password = event.target.elements.password.value;
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
 
-  if (email.trim() === "" || password.trim() === "") {
-    alert("Будь ласка, заповніть усі поля");
-    return;
+  if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
   }
 
-  console.log("Email:", email);
-  console.log("Password:", password);
-
-  event.target.reset();
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
 }
-
