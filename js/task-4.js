2811864b0c387168c86303d0e0a1b95589acec92
Viktor1907 <vikvelik@bigmir.net>
2024-02-24 13:43:16 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,6 @@
-const loginForm = document.querySelector(".login-form");
+const form = document.querySelector(".login-form");
 
-loginForm.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
